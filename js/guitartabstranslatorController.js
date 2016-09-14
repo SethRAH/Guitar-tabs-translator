4eaadf566b0975d0453a63e21256378183cda5d9
SethRAH <srahare@gmail.com>
2016-09-13 21:29:02 -0400

    small bit more angular
    - replaced some card filler with some angular model/view logic
    - added a swap card button.

--- js/guitartabstranslatorController.js
+++ js/guitartabstranslatorController.js
@@ -1,10 +1,32 @@
 (function(){
     "use strict";
 
-    angular.module("app-guitartabstranslator", guitarTabsTranslatorController);
+    angular.module("app-guitartabstranslator").controller("guitarTabsTranslatorController",guitarTabsTranslatorController);
 
     function guitarTabsTranslatorController(){
+        var vm = this;
 
+        vm.firstCard = {
+            "cardType": "tabsCard",
+            "options": [
+                { "name": "Guitar"},
+                { "name": "Bass"}
+            ]
+        };
+
+        vm.secondCard = {
+            "cardType": "scoreCard",
+            "options": [
+                { "name": "Trebble"},
+                { "name": "Bass"}
+            ]
+        };
+
+        vm.switchCards = function(){
+            var tempCard = vm.firstCard;
+            vm.firstCard = vm.secondCard;
+            vm.secondCard = tempCard;
+        };
     }
 
 })();
